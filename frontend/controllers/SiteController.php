2450647f32b326b163e61242981cb16550e2533d
Mikhail <adrej997@gmail.com>
2018-08-19 12:52:29 +0300

    logout fixed

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -106,7 +106,7 @@ class SiteController extends Controller
     {
         Yii::$app->user->logout();
 
-        return $this->goHome();
+        return $this->redirect('/');
     }
 
     public function actionSignup()
