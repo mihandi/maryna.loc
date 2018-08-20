543a34872691b14de6f97d0b40a6fbc3c1b341a5
Mikhail <adrej997@gmail.com>
2018-08-20 23:50:48 +0300

    category links fixed

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -43,7 +43,7 @@ return [
             'showScriptName' => false,
             'rules' => [
                 'blog/article/<seo-url:(\S+)>-<article_id:(\S+)>' => 'blog/article/',
-                'blog/category/<seo-url:(\S+)>-<category_id:(\S+)>' => 'blog/index/',
+                'blog/category/<seo-url:(\S+)>-<category_id:(\S+)>' => 'blog/category/',
                 'blog/search/<search:(\S+)>' => 'blog/search/',
             ],
         ],
